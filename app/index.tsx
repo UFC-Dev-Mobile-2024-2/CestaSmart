d04d7a62b832500f62aecd94a61002c1732601d5
Gabriela Machado Santos <108682563+mynameisgabu@users.noreply.github.com>
2025-03-07 16:33:39 +0000

    fix: rotas de navegação corrigidas

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,20 @@
-import { View, Text } from 'react-native';
+import { useEffect } from "react";
+import { useRouter, useRootNavigationState } from "expo-router";
+import { View, ActivityIndicator } from "react-native";
 
-export default function TelaHome() {
+export default function Index() {
+  const router = useRouter();
+  const navigationState = useRootNavigationState(); // Verifica se o layout está pronto
+
+  useEffect(() => {
+    if (!navigationState?.key) return; // Aguarda o layout ser montado
+
+    router.replace("/onboarding"); // Só redireciona após o layout estar pronto
+  }, [navigationState?.key]);
+
   return (
-    <View>
-      <Text>Tela Home</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ActivityIndicator size="large" color="#000" />
     </View>
   );
 }
