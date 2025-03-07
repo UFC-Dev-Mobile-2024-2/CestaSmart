60377c6a4ec4279b259808db5f7d98c226f3af89
Gabriela Machado Santos <108682563+mynameisgabu@users.noreply.github.com>
2025-03-07 09:04:08 +0000

    segunda versão onboarding

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import Routes from './routes';
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Routes />
-    </NavigationContainer>
-  );
-}
+const App = () => {
+  return <Routes />;
+};
+
+export default App;
